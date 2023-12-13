1a21979e91a523acdb200d9656f71db2bc1f68ae
presam6 <maglangit.mjosh@gmail.com>
2023-12-13 11:59:43 +0800

    add contents

--- App.js
+++ App.js
@@ -1,11 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
 
 export default function App() {
+const [stdId, setID] = useState('');
+const [name, setName] = useState('');
+const [age, setAge] = useState('');
+const [myData, addData] = useState([]);
+
+const addValues = () => {
+  let datas = myData;
+  myData.push({
+    'id' : stdId,
+    'name' : name,
+    'age' : age,
+  })
+  
+  addData(datas);
+  console.log(datas);
+  setID('');
+  setName(''); 
+  setAge('');
+}
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      <View>
+        <TextInput
+        placeholder='Student ID' onChangeText={setID}
+        />
+        <TextInput
+        placeholder='Student Name' onChangeText={setName}
+        />
+        <TextInput
+        placeholder='Student Age' onChangeText={setAge}
+        />
+      </View>
+
+      <View>
+        <Button
+        title='Submit' onPress={addValues}
+        />
+      </View>
+
+      <View style={{flex: 4, borderTopWidth: 5}}>
+        {
+          myData.map((obj, index) => <Text key={index}> {  obj.id + ', ' + obj.name + ', ' + obj.age }</Text>)
+        }
+      </View>
     </View>
   );
 }
@@ -13,8 +54,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 100,
+    alignContent: 'center',
   },
 });
